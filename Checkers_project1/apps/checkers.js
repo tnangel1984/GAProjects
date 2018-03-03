3290a1c30537d7c6e6dc087d2a7e682718a51773
tnangel1984 <tnangel1984123@gmail.com>
2018-03-02 19:13:17 -0500

    added Even Odd markers to square classes

--- Checkers_project1/apps/checkers.js
+++ Checkers_project1/apps/checkers.js
@@ -6,14 +6,25 @@ $(()=>{
 
     // Creates squares, used the row count * 8 to get to next row count + square count within row to get to square counts.
       $('body').append($('<container>'));
-       const $player1 =$('container').append($('<div>').addClass('player1'));
-       const $player2 =$('container').append($('<div>').addClass('player1'));
+      const $player1 =$('container').append($('<div>').addClass('player1'));
+      const $player2 =$('container').append($('<div>').addClass('player2'));
 
+    // creates Row & Square Divs
         for (i=0; i<8; i++){
-          $player1.append($('<div>').addClass('row').addClass('row'+(i+1)));
-          for(j=1; j<=8; j++){
-              $('.row'+(i+1)).append($('<div>').addClass('square').attr('id',8*i+j));
-          }
+          $('container').append($('<div>').addClass('row').addClass('row'+(i+1)));
+                // creates rows 1-8
+
+              for(j=1; j<=8; j++){
+                  let squareCount = i*8+j;
+
+                  if(squareCount%2 === 0){
+                       squareClass2 = 'even';
+                  }else{
+                      squareClass2 = 'odd';
+                  }  // determines squareClass2 = odd or even
+console.log(squareClass2);
+                  $('.row'+(i+1)).append($('<div>').addClass('square').addClass(squareClass2).attr('id',squareCount));
+              }   // creates squares 1-8 in each row ,
         }
 
 
