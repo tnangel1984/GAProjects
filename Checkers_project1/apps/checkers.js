117118aa44f446c53c85b8ee81b7b95812ebe7dc
tnangel1984 <tnangel1984123@gmail.com>
2018-03-02 19:59:07 -0500

    created oddEven function for efficiency

--- Checkers_project1/apps/checkers.js
+++ Checkers_project1/apps/checkers.js
@@ -1,39 +1,50 @@
 // console.log('test');
 // console.log($);
 
+// ====VARIABLES HERE====
+
+
+
+
+
+// ====FUNCTIONS HERE====
+
+const getEvenOdd = (countEvaluated, outputClass)=>{
+      if(countEvaluated%2 === 0){
+           return outputClass = 'even';
+      }else{
+          return outputClass = 'odd';
+      }  // determines squareClass2 = odd or even
+}
+
+
+
+
+
+
+
+
 // ====ON LOAD & ON CLICK EVENTS HERE====
 $(()=>{
 
-    // Creates squares, used the row count * 8 to get to next row count + square count within row to get to square counts.
+// creates a container,  creates player divs, appends to body
       $('body').append($('<container>'));
       const $player1 =$('container').append($('<div>').addClass('player1'));
       const $player2 =$('container').append($('<div>').addClass('player2'));
 
-    // creates Row & Square Divs
-        for (let rowCount=1; rowCount<=8; rowCount++){
-//Make this is into a functon that take rowClass or squareClass in as an argument then evaluates...
-          if(rowCount%2 === 0){
-               rowClass2 = 'even';
-          }else{
-              rowClass2 = 'odd';
-          }  // determines rowClass2 = odd or even
+// creates Row & Square Divs
+      let squareClass="";
+      let rowClass2="";
+      let squareClass2="";
 
-
-          $('container').append($('<div>').addClass('row').addClass(rowClass2).attr('id','row'+(rowCount+1)));
-                // creates rows 1-8
-
-
-
+              // creates rows 1-8
+      for (let rowCount=1; rowCount<=8; rowCount++){
+        rowClass2=getEvenOdd(rowCount,rowClass2);
+        $('container').append($('<div>').addClass('row').addClass(rowClass2+'Row').attr('id','row'+(rowCount+1)));
+            // Creates squares 1-8 per row, used the row count * 8 to get to next row count + square count in row to get to square counts.
               for(j=1; j<=8; j++){
                   let squareCount = (rowCount-1)*8+j;
-
-                  if(squareCount%2 === 0){
-                       squareClass2 = 'even';
-                  }else{
-                      squareClass2 = 'odd';
-                  }  // determines squareClass2 = odd or even
-
-
+                  squareClass2= getEvenOdd(squareCount, squareClass2);
                   $('#row'+(rowCount+1)).append($('<div>').addClass('square').addClass(squareClass2).attr('id',squareCount).text(squareCount));
               }   // creates squares 1-8 in each row ,
         }
@@ -50,7 +61,9 @@ $(()=>{
 })
 
 
-// ====FUNCTIONS HERE====
+
+
+
 
 //1.Build Board. 8x8
 //
