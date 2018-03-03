29a5b0a7fb2786717a4fe20faa4d4129aedb7737
tnangel1984 <tnangel1984123@gmail.com>
2018-03-02 19:34:56 -0500

    built checkerboard! Change method later

--- Checkers_project1/apps/checkers.js
+++ Checkers_project1/apps/checkers.js
@@ -10,20 +10,31 @@ $(()=>{
       const $player2 =$('container').append($('<div>').addClass('player2'));
 
     // creates Row & Square Divs
-        for (i=0; i<8; i++){
-          $('container').append($('<div>').addClass('row').addClass('row'+(i+1)));
+        for (let rowCount=1; rowCount<=8; rowCount++){
+//Make this is into a functon that take rowClass or squareClass in as an argument then evaluates...
+          if(rowCount%2 === 0){
+               rowClass2 = 'even';
+          }else{
+              rowClass2 = 'odd';
+          }  // determines rowClass2 = odd or even
+
+
+          $('container').append($('<div>').addClass('row').addClass(rowClass2).attr('id','row'+(rowCount+1)));
                 // creates rows 1-8
 
+
+
               for(j=1; j<=8; j++){
-                  let squareCount = i*8+j;
+                  let squareCount = (rowCount-1)*8+j;
 
                   if(squareCount%2 === 0){
                        squareClass2 = 'even';
                   }else{
                       squareClass2 = 'odd';
                   }  // determines squareClass2 = odd or even
-console.log(squareClass2);
-                  $('.row'+(i+1)).append($('<div>').addClass('square').addClass(squareClass2).attr('id',squareCount));
+
+
+                  $('#row'+(rowCount+1)).append($('<div>').addClass('square').addClass(squareClass2).attr('id',squareCount).text(squareCount));
               }   // creates squares 1-8 in each row ,
         }
 
